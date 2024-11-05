8e939bba6285dd3de7581a15654975fd62b1dcad
isolated_hwan <sksekwjdghks@hanmail.net>
2024-11-05 23:58:48 +0900

    7days

---
+++ 11.5/2. 쇠막대기.py
@@ -0,0 +1,15 @@
+s = input()
+stack = []
+cnt = 0
+for i in range(len(s)):
+    if s[i] == '(':
+        stack.append(i)
+    else:
+        if s[i-1] == '(':
+            stack.pop()
+            cnt += len(stack)
+        else:
+            stack.pop()
+            cnt += 1
+
+print(cnt)
