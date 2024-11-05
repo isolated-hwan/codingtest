3c711501e4640b290307d622519cfc0a6130969d
isolated_hwan <sksekwjdghks@hanmail.net>
2024-11-05 23:46:24 +0900

    7days

---
+++ 11.5/1.가장 큰 수.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+
+n = list(map(int, str(n)))
+
+stack = []
+for i in n:
+    while m > 0 and stack and stack[-1] < i:
+        stack.pop()
+        m -= 1
+    stack.append(i)
+
+if m > 0:
+    stack = stack[:-m]
+print(''.join(map(str, stack)))
